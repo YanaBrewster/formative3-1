8796064bfe90607aaa49924ab6623139c06d229d
YanaBrewster <yana.brewster@gmail.com>
2020-02-18 15:09:57 +1300

    ruby installed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
+
+  // TESTING JQUERY
   // $("button").click(function(){
   //   $("p").toggle();
   // });
 
+  var myKey = JSON.parse(apiKey);
+   console.log(myKey[0]);
+   myKey = myKey[0].key;
+
+   var endPoint;
 
 
 
@@ -32,5 +39,4 @@ $(document).ready(function(){
 
 
 
-  
 });

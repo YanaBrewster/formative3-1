4a81ad1b9cd5d0e04720d2b36b9df35024504734
YanaBrewster <yana.brewster@gmail.com>
2020-02-24 15:26:15 +1300

    added resgisterTasks to grunt file and edited about text

--- Gruntfile.js
+++ Gruntfile.js
@@ -103,6 +103,6 @@ module.exports = function(grunt) {
 
   // Default task(s).
   // grunt.registerTask('default', ['uglify']);
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass'], ['html'], ['imagemin'], ['cssmin'], ['uglify'], ['watch'], ['csslint'], ['jshint']);
 
 };
